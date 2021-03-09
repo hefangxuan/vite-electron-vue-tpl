fe45c3710d0da50187b6ea6c90354530ebe42cc9
hefangxuan <hefangxuan@qq.com>
2021-03-09 18:27:57 +0800

    - 完成基础vue 案列项目

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {useElectron} from "./use/electron";
+
+const {globalConfig} = useElectron()
+
+console.log('获取store: ', globalConfig.get('a'))
 
 createApp(App).mount('#app')
